ccd0bcd48b60575ef0753a0ef6aa8b52e82a8150
Łukasz Bułka <mckornik@poczta.fm>
2018-02-01 20:49:46 +0000

    Day4

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,14 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end('<h1>Hello World</h1>');
+
+});
+
+srv.listen(3000, () => {
+
+});
