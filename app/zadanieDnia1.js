f5375490ca2941ab52d98ac4c5ba7bb4238b8992
Jakub Matwiejew <custom@audiocustom.pl>
2018-02-01 22:36:52 +0100

    Dzień 4

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    let html = `Hello, World from back-end!`;
+    res.end(html, 'utf-8')
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
