0962363267c2cffa1bea30818af049f9d6b4a795
Greg Herbowicz <herbowicz@users.noreply.github.com>
2018-02-01 11:24:27 +0100

    Update zadanieDnia2.js

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,14 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.end(`<ul>
+    <li>Preferowane języki: ${req.headers['accept-language']}</li>
+    <li>User-agent: ${req.headers['user-agent']}</li>
+    <li>Accept: ${req.headers['accept']}</li>
+  </ul>`);
+});
+
+srv.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
