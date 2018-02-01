1f5528d9988f235232e7cbd55450aa84a9b83d76
RadekM86 <byrm.design@gmail.com>
2018-02-01 19:31:44 +0100

    Zadanie Dnia 2

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,18 @@
 //Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const acceptLanguage = req.headers['accept-language'];
+    const userAgent = req.headers['user-agent'];
+    console.log('Preferowane języki:', acceptLanguage);
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(`<h1>Hello, World from back-end!"</h1>
+        <table>
+        <tr><th>Preferowane języki</th><th>Przeglądarka</th></tr>
+        <tr><td>${acceptLanguage}</td><td>${userAgent}</td></tr>
+        </table>`)
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
