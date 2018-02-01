50dd1f832145426509c5d3605f821cdff8615274
Maciej Judka <maciej.judka@gmail.com>
2018-02-01 19:12:03 +0100

    Add zadanieDnia1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,10 @@
-//Twój kod
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.setHeader("Content-Type", "text/html; charset=utf-8");
+	res.end('Hello world!');
+});
+
+server.listen(3000, () => {
+	console.log('Server działa na porcie 3000');
+});
