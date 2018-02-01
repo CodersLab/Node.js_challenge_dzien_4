ee1895a22c7dbaa4f32a9e25e69bb5cea9742254
RadekM86 <byrm.design@gmail.com>
2018-02-01 19:38:21 +0100

    Poprawki w zad 2

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -4,12 +4,11 @@ const http = require('http');
 const srv = http.createServer((req, res) => {
     const acceptLanguage = req.headers['accept-language'];
     const userAgent = req.headers['user-agent'];
-    console.log('Preferowane języki:', acceptLanguage);
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end(`<h1>Hello, World from back-end!"</h1>
+    res.end(`<h1>Hello, World from back-end!</h1>
         <table>
         <tr><th>Preferowane języki</th><th>Przeglądarka</th></tr>
-        <tr><td>${acceptLanguage}</td><td>${userAgent}</td></tr>
+        <td>${acceptLanguage}</td><td>${userAgent}</td></tr>
         </table>`)
 });
 
