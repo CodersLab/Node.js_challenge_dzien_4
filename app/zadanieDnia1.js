e654d89f11ea5e75d4c4ad24cf74a4d1dc4e8238
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-02-01 22:32:13 +0100

    rozwiązania

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,9 @@
 //Twój kod
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'text/html; charset=utf8');
+  res.end('Hello, World from back-end!');
+});
+
+server.listen(3000);
