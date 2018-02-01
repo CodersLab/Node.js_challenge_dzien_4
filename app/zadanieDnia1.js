c47d36a56edda42d59be9aaa9ef6925c8831a3b3
Markowiecky <markowiecki.z@gmail.com>
2018-02-01 14:39:36 +0100

    Uzupełnione zadania - dzień 4

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req,res) =>{
+  console.log("Someone's at the door!");
+  res.setHeader('Content-Type', 'text/html; charset=uft-8');
+  res.end("<h1 style='text-align:center;'>Hello, World from back-end!</h1>");
+});
+
+srv.listen(3000, () => {
+  console.log('Serwer został uruchomiony na porcie 3000!');
+})
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,18 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req,res) =>{
+  console.log("Someone's at the door!");
+  const acceptLanguage = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+  const accept = req.headers['accept'];
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end('<ul>'
+  + '<li>Preferowane języki: '+ acceptLanguage+'</li>'
+  + '<li>System i przeglądarka to: '+ userAgent+'</li>'
+  + '<li>Przeglądarka preferuje: '+ accept+'</li>'
+  +'</ul>');
+});
+
+srv.listen(3000, () =>{
+  console.log('Serwer został uruchomiony na porcie 3000!');
+});
