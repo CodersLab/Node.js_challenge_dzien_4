0a45a4f5cf6a8f618bdb834d669ccd8b2cfe5ee0
Filip Godziński <godzinski.filip@gmail.com>
2018-02-02 15:13:16 +0100

    Zadanie Dnia 1 - done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,12 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1 style='text-align:center; color:blue;'>Hello, World from back-end!</h1>");
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
