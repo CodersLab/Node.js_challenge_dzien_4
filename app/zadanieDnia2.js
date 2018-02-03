7374e39dce0810a3949ed72f3bc738cfdd9eff9c
Gladkowski <marcingladkowski@gmail.com>
2018-02-03 16:51:39 +0100

    day 4

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const userAgent = req.headers['user-agent'];
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(userAgent);
+});
+
+server.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
