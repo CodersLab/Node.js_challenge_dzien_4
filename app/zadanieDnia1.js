3b1d60086bd38dc77d8474296ae2d8e0f22027af
ArkadiuszDz <arkadiusz.dziechciarz@gmail.com>
2018-02-05 20:47:06 +0100

    dzien 4

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,12 @@
 //Twój kod
+
+const http = require('http');
+const server = http.createServer((request,response) => {
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  response.end('Hello world from back-end!');
+
+});
+
+server.listen(3000,() => {
+  console.log('Serwer http uruchomiony na porcie 3000.');
+});
