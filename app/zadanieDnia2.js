9369ab7428f1a1af5f979e71b6f9360f843b7422
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-15 15:02:54 +0100

    Challenge done

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const srv = http.createServer((req, res) => {
     const userAgent = req.headers['user-agent'];
+    const accept = req.headers['accept'];
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end(userAgent);
+    res.end('<h1>Cześć przeglądarko!</h1>'+ '<p>Oto trochę informacji o Tobie: </p>' + userAgent + '<p>To są treści, jakie preferujesz: </p>' + accept);
 });
 
 srv.listen(3000, () => {
