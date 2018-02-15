4793a82f27544f776786fe80e678e64c630fcf35
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-15 14:56:58 +0100

    Ex. 02 done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,16 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const acceptLanguage = req.headers['accept-language'];
+    const userAgent = req.headers['user-agent'];
+    const accept = req.headers['accept'];
+    console.log('Preferowane języki:', acceptLanguage,
+        '\n info o systemie i przeglądarce użytkownika: ', userAgent,
+        '\n Treści preferowane przez przeglądarkę: ', accept);
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end('<html><body><h1>Hello, World from back-end!</h1></body></html>');
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const userAgent = req.headers['user-agent'];
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(userAgent);
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
