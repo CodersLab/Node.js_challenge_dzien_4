55429e8cfc7f8ae8ff71091a9a5481409b7bf9f0
Magdalena Goralczyk <magda.g10@wp.pl>
2018-07-05 23:24:17 +0200

    zadanie2

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http=require('http');
+
+const srv = http.createServer((req, res) => { //Kiedy przeglądarka wysyła zapytanie...
+    res.setHeader("Content-Type","text/html; charset=utf-8"); //...następuje ustawienie odpowiedniego kodowania...
+    const userAgent=req.headers['user-agent'];
+    res.end(userAgent); //...oraz przesłanie odpowiedzi.
+});
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
+//Coś tu powinno być...//Twój kod
