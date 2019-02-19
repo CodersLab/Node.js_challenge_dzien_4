0a6f2e1e82beb27be02d4770a1efbe9da7c4e6c1
Your Name <you@example.com>
2019-02-19 19:39:55 +0100

    task1 complete

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,12 @@
 //Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const acceptLanguage = req.headers['accept-language']
+    console.log('Preferowane języki: ', acceptLanguage)
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end('<h1>Hello World from back-end</h1>')
+})
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie http://localhost:3000/')
+})
